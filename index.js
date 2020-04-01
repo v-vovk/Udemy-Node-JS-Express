a5b701ba371dbdbe732cfd5919d4bd3eb3ffe89f
Vlad Vovk <wolfykey@gmail.com>
2020-04-01 19:11:04 +0300

    data rendering

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'Home page',
+    isHome: true
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('about')
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Add new courses',
+    isAdd: true
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Courses',
+    isCourses: true
+  })
 })
 
 const PORT = process.env.PORT || 3000
