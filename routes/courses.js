ed19cfe299b84cea4c62d43c174d60b49dbdd9dc
Vlad Vovk <wolfykey@gmail.com>
2020-04-02 19:45:28 +0300

    course edit

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,4 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const Course = require('../models/course')
 const router = Router()
 
@@ -9,6 +9,24 @@ router.get('/', async (req, res) => {
     isCourses: true,
     courses
   })
+})
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/')
+  }
+
+  const course = await Course.getById(req.params.id)
+
+  res.render('course-edit', {
+    title: `Edit ${course.title}`,
+    course
+  })
+})
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
 })
 
 router.get('/:id', async (req, res) => {
