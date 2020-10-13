6bee4bc1307043cf77e99c61d19593702422bc91
Vlad Vovk <wolfykey@gmail.com>
2020-10-13 22:17:44 +0300

    add items in cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,10 @@
 const { Router } = require('express')
-const Cart = require('../models/cart')
 const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Cart.add(course)
+  const course = await Course.findById(req.body.id)
+  await req.user.addToCart(course)
   res.redirect('/cart')
 })
 
@@ -16,13 +15,13 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-  const cart = await Cart.fetch()
-  res.render('cart', {
-    title: 'Cart',
-    isCart: true,
-    courses: cart.courses,
-    price: cart.price
-  })
+  // const cart = await Cart.fetch()
+  // res.render('cart', {
+  //   title: 'Cart',
+  //   isCart: true,
+  //   courses: cart.courses,
+  //   price: cart.price
+  // })
 })
 
 module.exports = router
